0eb97a159be3b1e319c8a336b2f266179a2849c1
FilippoToffoletto <filippotoffoletto@hotmail.it>
2023-01-18 17:17:50 +0100

    [add]aside menu admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.admin')
 
+@section('title')
+    | Admin
+@endsection
+
 @section('content')
 <div class="container mt-3">
     <h1>SIAMO NELLA DASHBOARD admin</h1>
---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,5 @@
+<nav class="">
+    <ul class="p-0 mt-3">
+        <li class="ft-nav-link mt-2"><a href="#" class="text-dark"><i class="fa-solid fa-table-columns me-1"></i> Dashboard</a></li>
+    </ul>
+</nav>
